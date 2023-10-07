1ad72997fb6d429e0c1101e8cadece148dbe6874
season8206 <127866331+season8206@users.noreply.github.com>
2023-10-08 07:57:13 +0800

    Add files via upload
    1

---
+++ therouterdemo/build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    //TheRouter
+    id ("cn.therouter.agp8") version "1.2.0-rc1" apply false
+    id ("com.google.devtools.ksp") version "1.5.31-1.0.0" apply false
+}
